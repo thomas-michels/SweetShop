c422fa5bf2f02a77a6830a601f4f01fc3754bde2
thomas-michels <thomasmichels.bnu@gmail.com>
2024-12-08 21:33:09 -0300

    fix: Fixing datetime parser

--- app/core/models/base_schema.py
+++ app/core/models/base_schema.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from pydantic import ConfigDict, BaseModel
 
 
@@ -8,8 +9,13 @@ def convert_field_to_camel_case(string: str) -> str:
     )
 
 
+def convert_datetime_to_realworld(dt: datetime) -> str:
+    return dt.replace(tzinfo=timezone.utc).isoformat().replace("+00:00", "Z")
+
+
 class GenericModel(BaseModel):
     model_config = ConfigDict(
         populate_by_name=True,
-        alias_generator=convert_field_to_camel_case
+        alias_generator=convert_field_to_camel_case,
+        json_encoders = {datetime: convert_datetime_to_realworld}
     )
