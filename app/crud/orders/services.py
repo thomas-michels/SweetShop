8068e4e59d3eecdb3b221d6d68b6d8b49ed8db48
Thomas Michels Rodrigues <thomas.rodrigues.contractor@filot.io>
2024-12-29 23:07:39 -0300

    fix: Fixing update delivery type

--- app/crud/orders/services.py
+++ app/crud/orders/services.py
@@ -79,7 +79,7 @@ class OrderServices:
                 if not await self.__tag_repository.select_by_id(id=tag, raise_404=False):
                     updated_order.tags.remove(tag)
 
-        if order_in_db.delivery.type == DeliveryType.DELIVERY and updated_order.delivery.type == DeliveryType.WITHDRAWAL:
+        if order_in_db.delivery.delivery_type == DeliveryType.DELIVERY and updated_order.delivery.delivery_type == DeliveryType.WITHDRAWAL:
             updated_order.delivery.delivery_value = 0
             updated_order.delivery.address = None
 
