dc19ffa50ebb45d8bd811286995ffc83d58d8861
thomas-michels <thomasmichels.bnu@gmail.com>
2025-02-16 00:13:54 -0300

    chore: Added redis cache for features

--- app/api/dependencies/get_plan_feature.py
+++ app/api/dependencies/get_plan_feature.py
@@ -1,12 +1,23 @@
 
+import json
+from app.api.dependencies.redis_manager import RedisManager
 from app.api.exceptions.authentication_exceptions import PaymentRequiredException
 from app.core.utils.features import Feature
 from app.crud.organization_plans.repositories import OrganizationPlanRepository
 from app.crud.plan_features.repositories import PlanFeatureRepository
 from app.crud.plan_features.schemas import PlanFeatureInDB
 
+redis_manager = RedisManager()
+
 
 async def get_plan_feature(organization_id: str, feature_name: Feature) -> PlanFeatureInDB:
+    cache_key = f"plan_feature:{organization_id}:{feature_name}"
+
+    cached_value = redis_manager.get_value(cache_key)
+
+    if cached_value:
+        return PlanFeatureInDB(**json.loads(cached_value))
+
     organization_plan_repository = OrganizationPlanRepository()
     plan_feature_repository = PlanFeatureRepository()
 
@@ -18,5 +29,7 @@ async def get_plan_feature(organization_id: str, feature_name: Feature) -> PlanF
         raise PaymentRequiredException()
 
     plan_feature = await plan_feature_repository.select_by_name(name=feature_name)
+
+    redis_manager.set_value(cache_key, plan_feature.model_dump_json(), expiration=600)
 
     return plan_feature
