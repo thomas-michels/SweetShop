6b422464dce05dad6080fde6d92129b62d93f5ed
thomas-michels <thomasmichels.bnu@gmail.com>
2025-03-06 23:27:07 -0300

    chore: sending emai

--- app/crud/marketing_emails/services.py
+++ app/crud/marketing_emails/services.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from app.api.dependencies.email_sender import send_email
+
 from .schemas import MarketingEmail, MarketingEmailInDB
 from .repositories import MarketingEmailRepository
 
@@ -12,7 +14,16 @@ class MarketingEmailServices:
     async def create(self, marketing_email: MarketingEmail) -> MarketingEmailInDB:
         marketing_email_in_db = await self.__marketing_email_repository.create(marketing_email=marketing_email)
 
-        # TODO send email here
+        with open("./templates/ebook-pascoa.html", mode="r", encoding="UTF-8") as file:
+            message = file.read()
+            message = message.replace("$CUSTOMER_NAME$", marketing_email_in_db.name)
+
+        send_email(
+            email_to=[marketing_email_in_db.email],
+            title='Obrigado por adquirir "Os sabores de ovos de Páscoa mais desejados do ano"! 🍫',
+            message=message
+        )
+
         return marketing_email_in_db
 
     async def search_by_id(self, id: str) -> MarketingEmailInDB:
