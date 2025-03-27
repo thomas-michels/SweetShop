0a7afe8806b3fad9d02354cefea8281ec0fc0c4b
thomas-michels <thomasmichels.bnu@gmail.com>
2025-03-27 01:42:22 -0300

    chore: updating again

--- app/crud/shared_schemas/operating_days.py
+++ app/crud/shared_schemas/operating_days.py
@@ -1,15 +1,15 @@
 import re
+from typing import List
 from pydantic import Field, model_validator
 from app.core.models.base_schema import GenericModel
 
 
-class OperatingDay(GenericModel):
-    day: int = Field(example=5)
+class IntervalWork(GenericModel):
     start_work: str = Field(example="10:10")
     end_work: str = Field(example="12:00")
 
     @model_validator(mode="after")
-    def validate_work_time(self) -> "OperatingDay":
+    def validate_work_time(self) -> "IntervalWork":
         time_pattern = r'^([01]?[0-9]|2[0-3]):[0-5][0-9]$'
 
         if not re.match(time_pattern, self.start_work):
@@ -19,3 +19,8 @@ class OperatingDay(GenericModel):
             raise ValueError(f"end_work '{self.end_work}' não está no formato hh:mm válido (00:00-23:59)")
 
         return self
+
+
+class OperatingDay(GenericModel):
+    day: int = Field(example=5)
+    intervals: List[IntervalWork] = Field(default=[])
