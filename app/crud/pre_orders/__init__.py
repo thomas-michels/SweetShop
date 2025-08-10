b3d20874ec33f6ed6587e2e8161c4a8df8a44088
Thomas Michels <57485730+thomas-michels@users.noreply.github.com>
2025-08-10 01:11:58 -0300

    fix: export pre-order services

--- app/crud/pre_orders/__init__.py
+++ app/crud/pre_orders/__init__.py
@@ -1,7 +1,9 @@
 from .schemas import PreOrderStatus, PreOrderInDB, UpdatePreOrder
+from .services import PreOrderServices
 
 __all__ = [
     "PreOrderStatus",
     "PreOrderInDB",
     "UpdatePreOrder",
+    "PreOrderServices",
 ]
