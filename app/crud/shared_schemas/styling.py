546c499232cfef6365f0c4b7d8306b332b0de5b3
thomas-michels <thomasmichels.bnu@gmail.com>
2025-09-30 23:59:20 -0300

    fix: Fixed tag color

--- app/crud/shared_schemas/styling.py
+++ app/crud/shared_schemas/styling.py
@@ -5,3 +5,4 @@ from pydantic import Field
 class Styling(GenericModel):
     primary_color: str | None = Field(default=None, example="#000000")
     secondary_color: str | None = Field(default=None, example="#111111")
+    font_color: str | None = Field(default=None, example="#111111")
